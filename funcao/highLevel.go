5bb35b1423cd8a72e31d6871417bfb2e93894b54
Thomaz Henrique <treimberg7@gmail.com>
2021-06-13 18:38:37 -0300

    .

--- funcao/highLevel.go
+++ funcao/highLevel.go
@@ -1,7 +1,29 @@
+// Thomaz
 package funcao
 
-func E1_FuncoesParaNumeros() {
+import (
+	"fmt"
+	"math"
+)
 
+func E1_FuncoesParaNumeros() {
+	var x float64 = 10.1234
+	var y float64 = 9
+
+	var x1 float64 = math.Floor(x)
+	fmt.Println(x1)
+	var x2 float64 = math.Ceil(x)
+	fmt.Println(x2)
+	var x3 float64 = math.Trunc(x)
+	fmt.Println(x3)
+	var x4 float64 = math.Round(x)
+	fmt.Println(x4)
+	var x5 float64 = math.Abs(x)
+	fmt.Println(x5)
+	var x6 float64 = math.Sqrt(x)
+	fmt.Println(x6)
+	var x7 float64 = math.Pow(y, 2)
+	fmt.Println(x7)
 }
 
 func E2_FuncoesParaTextos() {
--- tipo/primitive.go
+++ tipo/primitive.go
@@ -1,3 +1,4 @@
+// Thomaz
 package tipo
 
 import (
