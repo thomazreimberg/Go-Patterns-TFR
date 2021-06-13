b01b22d291e80d0817a7395452be7505587bceff
Thomaz Henrique <treimberg7@gmail.com>
2021-06-13 20:30:24 -0300

    Update procedure.go

--- funcao/procedure.go
+++ funcao/procedure.go
@@ -1,30 +1,54 @@
 //Thomaz
 package funcao
 
-func E1_AreaRetangulo() {
+import (
+	"math"
+	"strings"
+	"time"
+)
 
+type Retangulo struct {
+	Altura int
+	Base int
 }
 
-func E2_Media() {
-
+type Bhaskara struct {
+	X1 float64
+	X2 float64
 }
 
-func E3_Hipontenusa() {
-
+func E1_AreaRetangulo(a int, b int) int {
+	return a * b
 }
 
-func E4_PrimeiroNome() {
-
+func E2_Media(a float64, b float64, c float64) float64 {
+	return (a + b + c) / 3
 }
 
-func E5_PrimeiroDiaProximoMes() {
-
+func E3_Hipontenusa(co float64, ca float64) float64 {
+	return math.Sqrt(math.Pow(ca, 2) + math.Pow(co, 2))
 }
 
-func E6_AreaRetangulo() {
-
+func E4_PrimeiroNome(nome string) string {
+	p := nome[0 : strings.Index(nome, " ")]
+	return p
 }
 
-func E7_Bhaskara() {
+func E5_PrimeiroDiaProximoMes(dia time.Time) time.Time {
+	return dia.AddDate(0, 1, 0).AddDate(0, 0, -dia.Day() + 1)
+}
 
+func E6_AreaRetangulo(ret Retangulo) int {
+	return ret.Altura * ret.Base
+}
+
+func E7_Bhaskara(a int, b int, c int) Bhaskara {
+	//delta := math.Pow(b, 2) - 4 * a * c
+	delta := (math.Pow(float64(b), 2) - float64(4 * a * c))
+	x := Bhaskara {
+		X1: (-float64(b) + delta) / float64((2 * a)),
+		X2: (-float64(b) - delta) / float64((2 * a)),
+	}
+
+	return x
 }
